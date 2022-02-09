c707467971fbd40e9e7825b5496767fa5813e104
abhishek36 <akamble9913@gmail.com>
2022-02-09 14:41:27 +0530

    first commit

---
+++ src/controllers/bookmark.controller.js
@@ -0,0 +1,15 @@
+//import Bookmark model here
+
+const addOrRemoveBookmark = async (req, res) => {
+  //addOrRemoveBookmark api logic here
+};
+
+const getBookmarkByUserId = async (req, res) => {
+  //getBookmarkByUserId api logic here
+};
+
+const BookmarkController = {
+  addOrRemoveBookmark,
+  getBookmarkByUserId,
+};
+module.exports = BookmarkController;
---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,8 @@
+const uploadFile = async (req, res) => {
+  //uploadFile api logic here the life should pe store in public/uploads folder
+};
+
+const uploadController = {
+  uploadFile,
+};
+module.exports = uploadController;
